c83954947093569b2ceb31cfa80bf0e1d664488d
mishalp <mishalclever@gmail.com>
2022-07-28 10:22:44 +0530

    bug fixes and improvements

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ function div(num1,num2){
 }
 
 result = 0
-
+let tempDig1 = ""
+let tempDig2 = ""
+let tempOp = ""
 function operate(num1,num2,op,opNew){
     if(op == '+'){
         result = sum(num1,num2)
@@ -24,8 +26,12 @@ function operate(num1,num2,op,opNew){
         result = div(parseFloat(num1),parseFloat(num2))
     }
     document.getElementById('side').value = result
+    tempDig1 = digit1
+    tempDig2 = digit2
+    tempOp = operator
     digit1 = result
     digit2 = ""
+    operator2Clicked = false
     if(opNew == "="){
         document.getElementById('display').value = result;
         document.getElementById('side').value = ""
@@ -43,6 +49,7 @@ function operate(num1,num2,op,opNew){
 let newOp = ""
 let digit1 = ""
 let operator1Clicked = false
+let operator2Clicked = false
 let operator = ""
 let dig1Full = false
 let digit2 = ""
@@ -65,6 +72,7 @@ function btnClick(val){
     }else{
         document.getElementById('dec').disabled = false
         if(operator1Clicked){
+            operator2Clicked = true
             newOp = val
                 operate(parseFloat(digit1),parseFloat(digit2),operator,newOp)
 
@@ -83,13 +91,8 @@ function btnClick(val){
 
 
 function eql() {
-    if(dec){
-        operate(parseFloat(digit1),parseFloat(digit2),operator,"=")
-
-    }else{
-        operate(parseInt(digit1),parseInt(digit2),operator,"=")
-    }
-    
+       operate(parseFloat(digit1),parseFloat(digit2),operator,"=")
+       document.getElementById('dec').disabled = false
 }
 
 
@@ -98,7 +101,8 @@ function eql() {
 function clr(){
     document.getElementById('display').value =""
     document.getElementById('dec').disabled = false
-    operatorClicked = false
+    operator1Clicked = false
+    operator2Clicked = false
     digit1 = ""
     digit2 = ""
     dig1Full = false
@@ -106,4 +110,39 @@ function clr(){
     newOp = ""
     result = 0
     dec = false
+}
+let str = ""
+function dlt(){
+
+    str = document.getElementById('display').value 
+    let dltVal = str.slice(str.length-1)
+    document.getElementById('display').value = str.slice(0, str.length-1)
+    if(!(dltVal == '+' || dltVal == '-' || dltVal == '*' || dltVal == '/')){
+        if(digit2){
+            digit2 = digit2.slice(0 ,digit2.length-1)
+        }else{
+            digit1 = digit1.slice(0 ,digit1.length-1)
+        }
+        if(dltVal == '.'){
+            dec = false
+            document.getElementById('dec').disabled = false
+        }
+    }else{
+        if(operator2Clicked){
+            operator2Clicked = false
+                digit1 = tempDig1
+                digit2 = tempDig2
+                operator = tempOp
+                newOp = ""
+                document.getElementById('side').value = ""
+
+
+        }else{
+            operator1Clicked = false
+            operator = ""
+            dig1Full = false
+
+        }
+
+    }
 }
